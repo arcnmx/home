0f7cbc22a595802ac71614356587f2018c6b79cf
arcnmx <arcnmx@users.noreply.github.com>
2019-04-25 14:52:58 -0700

    paswitch alias

--- config/profiles/host/shanghai/home.nix
+++ config/profiles/host/shanghai/home.nix
@@ -10,6 +10,23 @@
 
     home.packages = [ ]; # TODO: this
     services.konawall.tags = ["score:>=200" "width:>=1600" "rating:safe"];
+    home.shell.functions.paswitch = ''
+      case $1 in
+        speakers)
+          ${pkgs.paswitch.exec} alsa_output.pci-0000_20_00.3.analog-stereo
+          ;;
+        headphones)
+          ${pkgs.paswitch.exec} alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
+          ;;
+        "")
+          echo "paswitch {speakers|headphones|SINK_NAME}" >&2
+          return 1
+          ;;
+        *)
+          ${pkgs.paswitch.exec} "$@"
+          ;;
+      esac
+    '';
 
     systemd.user.services.getquote = {
       Unit = {
