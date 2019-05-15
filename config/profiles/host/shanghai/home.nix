0be1085a798dacad6989632743aa544242ae950a
arcnmx <arcnmx@users.noreply.github.com>
2019-05-15 16:36:21 -0700

    paswitch zsh completion

--- config/profiles/host/shanghai/home.nix
+++ config/profiles/host/shanghai/home.nix
@@ -10,22 +10,33 @@
 
     home.packages = [ ]; # TODO: this
     services.konawall.tags = ["score:>=200" "width:>=1600" "rating:safe"];
-    home.shell.functions.paswitch = ''
-      case $1 in
-        speakers)
-          ${pkgs.paswitch.exec} alsa_output.pci-0000_20_00.3.analog-stereo
-          ;;
-        headphones)
-          ${pkgs.paswitch.exec} alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
-          ;;
-        "")
-          echo "paswitch {speakers|headphones|SINK_NAME}" >&2
-          return 1
-          ;;
-        *)
-          ${pkgs.paswitch.exec} "$@"
-          ;;
-      esac
+    home.shell.functions = {
+      paswitch = ''
+        case $1 in
+          speakers)
+            ${pkgs.paswitch.exec} alsa_output.pci-0000_20_00.3.analog-stereo
+            ;;
+          headphones)
+            ${pkgs.paswitch.exec} alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
+            ;;
+          "")
+            echo "paswitch {speakers|headphones|SINK_NAME}" >&2
+            return 1
+            ;;
+          *)
+            ${pkgs.paswitch.exec} "$@"
+            ;;
+        esac
+      '';
+      _paswitch_sinks = ''
+        ${pkgs.pulseaudio}/bin/pactl list short sinks | ${pkgs.coreutils}/bin/cut -d $'\t' -f 2
+      '';
+      _paswitch = ''
+        _alternative 'preset:preset:(headphones speakers)' 'sink:sink:($(_paswitch_sinks))'
+      '';
+    };
+    programs.zsh.initExtra = ''
+      compdef _paswitch paswitch
     '';
 
     systemd.user.services.getquote = {
