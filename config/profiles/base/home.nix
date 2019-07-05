3fa83ecc552c5335ae39343ae5550dd43b661e34
arcnmx <arcnmx@users.noreply.github.com>
2019-07-05 12:35:30 -0700

    start messing with kak

--- config/profiles/base/home.nix
+++ config/profiles/base/home.nix
@@ -336,6 +336,79 @@ in {
       '';
     };
 
+    programs.kakoune = {
+      enable = true;
+      config = {
+        tabStop = 4;
+        indentWidth = 0; # default = 4, 0 = tabs
+        scrollOff = {
+          lines = 4;
+          columns = 4;
+        };
+        ui = {
+          setTitle = false; # or true? depends if you can control format, I just want the filename
+          statusLine = "bottom";
+          assistant = "cat";
+          enableMouse = true; # hmmmm
+          changeColors = true; # what is this?
+          wheelDownButton = null;
+          wheelUpButton = null;
+          #shiftFunctionKeys = 12; # um is this useful?
+          useBuiltinKeyParser = false; # what's this for?
+        };
+        showMatching = true;
+        wrapLines = {
+          enable = true;
+          word = true;
+          indent = true; # this is probably too weird
+          marker = "↪"; # ↳
+        };
+        numberLines = {
+          enable = true;
+          relative = true;
+          highlightCursor = true;
+          #separator = "";
+        };
+        showWhitespace = {
+          enable = true;
+          nonBreakingSpace = "·";
+          tab = "»";
+        };
+        keyMappings = [
+          {
+            mode = "insert";
+            docstring = "completion nav down";
+            key = "<a-j>";
+            effect = "<c-n>";
+          }
+          {
+            mode = "insert";
+            docstring = "completion nav up";
+            key = "<a-k>";
+            effect = "<c-p>";
+          }
+        ];
+        hooks = [
+          /*{
+            name = "NormalBegin";
+            once = false;
+            group = "group";
+            option = "filetype=rust";
+            commands = ''
+            '';
+          }*/
+        ];
+      };
+      plugins = with pkgs.kakPlugins; [
+        kak-crosshairs
+        kakoune-registers
+        explore-kak
+        fzf-kak
+        kak-lsp
+        kak-tree
+      ];
+      #extraConfig = "";
+    };
     programs.vim = {
       enable = true;
       packageConfigurable = pkgs.vim_configurable-pynvim;
