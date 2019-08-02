d7cef8a2ba4ab2aeb0fa0eaae41c7d3d475a5f81
arcnmx <arcnmx@users.noreply.github.com>
2019-08-02 09:58:23 -0700

    firefox config and tridactyl cleanup

--- config/modules/home/default.nix
+++ config/modules/home/default.nix
@@ -2,11 +2,13 @@
   disabledModules = [
     "programs/git.nix"
     "programs/vim.nix"
+    "programs/firefox.nix"
   ];
 
   imports = [
     ./hm_git.nix
     ./hm_vim.nix
+    ./hm_firefox.nix
     ./yggdrasil-7n.nix
   ];
 }
