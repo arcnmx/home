37dade184ef47d94de6753af70dd5a3c943f1c88
arcnmx <arcnmx@users.noreply.github.com>
2019-08-18 21:34:02 -0700

    fix up laptop network shortcuts

--- config/profiles/laptop/home.nix
+++ config/profiles/laptop/home.nix
@@ -7,13 +7,21 @@
     home.shell.functions = {
       bluenet = ''
         ${pkgs.systemd}/bin/systemctl start bluetooth
+        ${pkgs.connman}/bin/connmanctl enable bluetooth
         ${pkgs.connman}/bin/connmanctl disable wifi
+        ${pkgs.systemd}/bin/systemctl restart connman
       '';
       iosnet = ''
         ${pkgs.systemd}/bin/systemctl restart usbmuxd
         ${pkgs.connman}/bin/connmanctl disable wifi
+        ${pkgs.connman}/bin/connmanctl disable bluetooth
         sleep 2.5
         ${pkgs.connman}/bin/connmanctl connect ethernet_f2796070f331_cable
+      '';
+      winet = ''
+        ${pkgs.connman}/bin/connmanctl disable bluetooth
+        ${pkgs.connman}/bin/connmanctl enable wifi
+        ${pkgs.connman}/bin/connmanctl scan wifi
       '';
     };
 
