5606ca2f23f1670cb1edeaa5b293d6150c9a8eb3
arcnmx <arcnmx@users.noreply.github.com>
2019-10-08 11:09:02 -0700

    oh that setting is unreleased...

--- config/profiles/base/home.nix
+++ config/profiles/base/home.nix
@@ -181,6 +181,9 @@ in {
         target = "${config.xdg.cacheHome}/cargo/git/";
         create = true;
       };
+      # need this at least until a new coc-nvim version is released...
+      ".vim".target = "${config.xdg.configHome}/vim/";
+      ".vim/coc-settings.json".target = "${config.xdg.configHome}/vim/coc/coc-settings.json"; # and this too :(
     };
     home.language = {
       base = "en_US.UTF-8";
