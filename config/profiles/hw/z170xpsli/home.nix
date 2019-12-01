472afd00a491809bde659b451b48a127858d3deb
arcnmx <arcnmx@users.noreply.github.com>
2019-11-30 17:01:22 -0800

    goodbye old stuff

--- config/profiles/hw/z170xpsli/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }: with lib; {
-  options = {
-    home.profiles.hw.z170xpsli = mkEnableOption "GIGABYTE Z170 XP SLI";
-  };
-
-  config = mkIf config.home.profiles.hw.z170xpsli {
-    home.profiles.hw.intel = true;
-  };
-}
