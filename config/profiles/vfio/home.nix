8a2a057e8adecb3bc800a8cc9758d93cf17bacc6
arcnmx <arcnmx@users.noreply.github.com>
2020-03-17 10:14:38 -0700

    shorthand vm alias

--- config/profiles/vfio/home.nix
+++ config/profiles/vfio/home.nix
@@ -1,12 +1,21 @@
-{ config, pkgs, lib, ... }: with lib; {
+{ config, pkgs, lib, ... }: with lib; let
+  windows = pkgs.writeShellScriptBin "windows" ''
+    tmux new-session -d -s windows \
+      "cd ~/projects/arc.github/vfio; echo vm windows run; $SHELL -i" \; \
+      split-window -h "top -H" \; \
+      split-window -v "watch -t 'cat /proc/cpuinfo|grep MHz'" \; \
+      select-pane -L \; \
+      split-window -dv "watch -t sensors -c /etc/sensors3.conf" \; \
+      attach
+  '';
+in {
   options = {
     home.profiles.vfio = mkEnableOption "VFIO";
   };
 
   config = mkIf config.home.profiles.vfio {
     home.packages = [
-      #arc.ovmf-macboot
-      #arc.qemu-headless
+      windows
     ];
   };
 }
