063a70d53932311dcfbe78a2166de6da054a8005
arcnmx <arcnmx@users.noreply.github.com>
2020-11-01 11:40:53 -0800

    patoggle

--- config/profiles/host/shanghai/home.nix
+++ config/profiles/host/shanghai/home.nix
@@ -1,6 +1,27 @@
-{ config, pkgs, lib, ... }: with lib; {
+{ config, pkgs, lib, ... }: with lib; let
+  patoggle = pkgs.writeShellScriptBin "patoggle" ''
+    set -eu
+    SINKS=(headphones speakers)
+
+    DEFAULT_SINK=$(${pkgs.pulseaudio}/bin/pactl info | ${pkgs.gnugrep}/bin/grep Sink | ${pkgs.coreutils}/bin/cut -d ' ' -f 3)
+    SINK_INDEX=0
+    for sink in "''${SINKS[@]}"; do
+      ((++SINK_INDEX))
+      if [[ $sink == $DEFAULT_SINK ]]; then
+        break
+      fi
+    done
+    SINK_INDEX=$((SINK_INDEX % ''${#SINKS[@]}))
+
+    exec ${pkgs.paswitch}/bin/paswitch ''${SINKS[$SINK_INDEX]}
+  '';
+in {
   options = {
     home.profiles.host.shanghai = mkEnableOption "hostname: shanghai";
+    home.programs.paswitch.patoggle = mkOption {
+      type = types.package;
+      default = patoggle;
+    };
   };
 
   config = mkMerge [ {
@@ -13,7 +34,7 @@
 
     xdg.configFile."i3status/config".source = ./files/i3status;
 
-    home.packages = [ pkgs.paswitch ];
+    home.packages = [ pkgs.paswitch config.home.programs.paswitch.patoggle ];
     services.konawall.tags = ["score:>=200" "width:>=1600" "rating:safe"];
     home.shell.functions = {
       _paswitch_sinks = ''
