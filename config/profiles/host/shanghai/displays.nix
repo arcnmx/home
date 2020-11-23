162d8f2d0293bf6a88b1b8f4f854df8a3afbe83d
arcnmx <arcnmx@users.noreply.github.com>
2020-11-23 11:52:11 -0800

    fix linear display layout

--- config/profiles/host/shanghai/displays.nix
+++ config/profiles/host/shanghai/displays.nix
@@ -39,7 +39,7 @@
         width = 2560;
         height = 1440;
         x = 0;
-        y = dell.viewport.height - config.viewport.height;
+        y = dell.y + dell.viewport.height - config.viewport.height;
       };
       dell = { config, ... }: {
         output = "HDMI-0";
@@ -59,6 +59,38 @@
         width = 3840;
         height = 2160;
         x = dell.x + dell.viewport.width;
+        y = 0;
+      };
+    };
+    gaming = monitors: with monitors; {
+      # linear but with benq in the middle
+      benq = { config, ... }: {
+        output = "DP-0";
+        xserver.sectionName = "Monitor[1]";
+        nvidia.options.AllowGSYNCCompatible = mkIf (hasPrefix "DP-" config.output) "On";
+        width = 2560;
+        height = 1440;
+        x = dell.x + dell.viewport.width;
+        y = dell.y + dell.viewport.height - config.viewport.height;
+      };
+      dell = { config, ... }: {
+        output = "HDMI-0";
+        xserver.sectionName = "Monitor[0]";
+        nvidia.options.AllowGSYNCCompatible = mkIf (hasPrefix "DP-" config.output) "On";
+        primary = true;
+        width = 3840;
+        height = 2160;
+        x = 0;
+        y = lg.viewport.height - config.viewport.height;
+      };
+      lg = { config, ... }: {
+        output = "DP-2";
+        xserver.sectionName = "Monitor[2]";
+        nvidia.options.AllowGSYNCCompatible = mkIf (hasPrefix "DP-" config.output) "On";
+        rotation = "right";
+        width = 3840;
+        height = 2160;
+        x = benq.x + benq.viewport.width;
         y = 0;
       };
     };
