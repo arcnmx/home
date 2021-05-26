d468d03fb351d12809001e1359f9f136b18646d1
arcnmx <arcnmx@users.noreply.github.com>
2021-05-25 22:56:16 -0700

    some ryzen updates

--- config/profiles/hw/ryzen/nixos.nix
+++ config/profiles/hw/ryzen/nixos.nix
@@ -5,25 +5,15 @@
 
   config = mkIf config.home.profiles.hw.ryzen {
     boot = {
-      kernel.sysctl = {
-        "kernel.randomize_va_space" = 0; # related to https://bugzilla.kernel.org/show_bug.cgi?id=196683
-      };
       kernelParams = ["amd_iommu=on"];
       kernelModules = [
         "msr" # for zenstates
+        "ryzen_smu" # for ryzen-monitor
         "kvm-amd"
       ];
-      kernelPatches = optional (config.home.profiles.vfio && false) {
-        # "Error: internal error: Unknown PCI header type ‘127’"
-        # (though fixed in AGESA 1.0.0.4B, that update breaks RAM training so continue to use this patch until there's a better/stable version to update to)
-        # As of 2019-12-03 there is a fixed version for my board and this is no longer necessary
-        name = "pcie-reset-fix";
-        patch = (pkgs.fetchurl {
-          url = "https://clbin.com/VCiYJ";
-          sha256 = "0xap49fn3287r8lb3xpayzmvmiy1hdv5ijalgh4052wn62mmh30k";
-        });
-      };
+      extraModulePackages = with config.boot.kernelPackages; [ ryzen-smu zenpower ];
     };
+    hardware.cpu.amd.updateMicrocode = true;
     environment.etc = {
       "sensors3.conf".text = ''
         chip "k10temp-pci-00c3"
@@ -32,7 +22,8 @@
     };
     environment.systemPackages = with pkgs; [
       lm_sensors
-      #zenstates
+      ryzen-smu-monitor_cpu
+      ryzen-monitor
     ];
   };
 }
--- config/profiles/hw/x570am/nixos.nix
+++ config/profiles/hw/x570am/nixos.nix
@@ -7,11 +7,18 @@
     home.profiles.hw.ryzen = true;
 
     boot = {
+      kernelModules = [ "it87" ];
+      extraModulePackages = with config.boot.kernelPackages; [ it87 ];
       initrd.availableKernelModules = [
         "nvme" "sd_mod" "xhci_pci" "ahci" "usbhid"
       ];
-      modprobe.modules.snd_hda_intel.options = {
-        model = "dual-codecs";
+      modprobe.modules = {
+        snd_hda_intel.options = {
+          model = "dual-codecs";
+        };
+        it87.options = {
+          force_id = "0x8628";
+        };
       };
     };
     hardware.bluetooth = {
@@ -27,6 +34,24 @@
       wirelesstools
       bluez
     ];
+    environment.etc = {
+      "sensors3.conf".text = ''
+        chip "it8628-isa-0a40"
+          label temp1 "System 1"
+          label temp2 "Chipset"
+          label temp3 "CPU Socket"
+          label temp4 "PCIEX16"
+          label temp5 "VRM MOS"
+          label temp6 "VSOC MOS"
+          label in0 "CPU Vcore"
+          label in1 "+3.3V"
+          label in2 "+12V"
+          label in3 "+5V"
+          label in4 "CPU Vcore SOC"
+          label in5 "CPU Vddp"
+          label in6 "DRAM A/B"
+      '';
+    };
     systemd.network = {
       networks.eno1 = {
         inherit (config.systemd.network.links.eth) matchConfig;
