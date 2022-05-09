f52e14b2a73109d5fdb1f4dabacedfedd967ffbb
arcnmx <arcnmx@users.noreply.github.com>
2022-05-09 11:29:56 -0700

    syncplay formats

--- config/profiles/gui/home.nix
+++ config/profiles/gui/home.nix
@@ -907,6 +907,7 @@ in {
       defaultRoom = "lounge";
       gui = false;
       trustedDomains = [ "youtube.com" "youtu.be" "twitch.tv" "soundcloud.com" ];
+      playerArgs = singleton ''--ytdl-format=bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/bestvideo+bestaudio/best'';
       config = {
         client_settings = {
           autoplayrequiresamefiles = false;
