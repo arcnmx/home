05af7e6a40b0d0eea61156389944c362c1c68735
arcnmx <arcnmx@users.noreply.github.com>
2022-10-03 14:23:58 -0700

    support vfio disks with 4k sector sizes

--- cfg/vfio/scripts/map-disk.sh
+++ cfg/vfio/scripts/map-disk.sh
@@ -21,26 +21,30 @@ fi
 
 MBR_FILE=$RUNTIME_DIRECTORY/mbr
 BLOCKDEV=$(basename $(readlink $DISK))
+PARENTDEV=$(lsblk -no pkname $DISK | tail -n1)
+PARENT_SECTOR_SIZE=$(cat /sys/class/block/$PARENTDEV/queue/hw_sector_size || echo 512)
 BLOCK_START=$(cat /sys/class/block/$BLOCKDEV/start)
-BLOCK_SIZE=$(cat /sys/class/block/$BLOCKDEV/size)
-DISK_SIZE=$(((BLOCK_SIZE + 2048 + 2047) / 2048 * 2048))
+BLOCK_SIZE_512S=$(cat /sys/class/block/$BLOCKDEV/size)
+# 2048 * 512-byte sectors = 1MB alignment
+DISK_SIZE_512S=$(((BLOCK_SIZE_512S + 2048 + 2047) / 2048 * 2048))
+BS=
 dd if=/dev/zero of=$MBR_FILE bs=512 count=2048 status=none
 
 LDEV=$(losetup --show -f $MBR_FILE)
 echo $LDEV > $RUNTIME_DIRECTORY/loop
 
-END_GUARD=$((DISK_SIZE - 2048 - BLOCK_SIZE))
+END_GUARD=$((DISK_SIZE_512S - 2048 - BLOCK_SIZE_512S))
 if [[ $END_GUARD -ne 0 ]]; then
-	END_GUARD="$((BLOCK_SIZE + 2048)) $END_GUARD zero"
+	END_GUARD="$((BLOCK_SIZE_512S + 2048)) $END_GUARD zero"
 else
 	END_GUARD=
 fi
 dmsetup create $DM_NAME <<EOF
 0 2048 linear $LDEV 0
-2048 $BLOCK_SIZE linear $DISK 0
+2048 $BLOCK_SIZE_512S linear $DISK 0
 $END_GUARD
 EOF
-CYL=$((DISK_SIZE / 2048))
+CYL=$((DISK_SIZE_512S / 2048))
 if [[ $CYL -gt 65535 ]]; then
 	CYL=65535
 fi
@@ -50,8 +54,8 @@ label: dos
 label-id: 0x${DISK_ID::8}
 device: $DM_NAME
 unit: sectors
-grain: 512
-sector-size: 512
+grain: $PARENT_SECTOR_SIZE
+sector-size: $PARENT_SECTOR_SIZE
 
-1 : start=2048, size=$BLOCK_SIZE, type=$DISK_TYPE
+1 : start=$((2048*512/PARENT_SECTOR_SIZE)), size=$((BLOCK_SIZE_512S*512/PARENT_SECTOR_SIZE)), type=$DISK_TYPE
 EOF
