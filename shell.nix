916cea16087810bfe59719c578aea0a36a99b350
arcnmx <git@git.arcn.mx>
2022-10-16 15:24:49 -0700

    fix esphome font component

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
 { pkgs ? import <nixpkgs> { } }: with pkgs; let
+  esphome = pkgs.esphome.overridePythonAttrs (old: {
+    propagatedBuildInputs = old.propagatedBuildInputs ++ [ python3Packages.requests ];
+  });
 in mkShell {
   nativeBuildInputs = [ esphome gnumake ];
 }
