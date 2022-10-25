146a34ec44233caeae580e32edccc13f70fea0b8
arcnmx <git@git.arcn.mx>
2022-10-25 13:04:21 -0700

    pull in font via nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }: with pkgs; let
-  esphome = pkgs.esphome.overridePythonAttrs (old: {
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [ python3Packages.requests ];
-  });
+{ pkgs ? import <nixpkgs> { } }: with pkgs; with lib; let
+  fonts = pkgs.linkFarm "esphome-fonts" [
+    {
+      name = "cozette.bdf";
+      path = "${cozette}/share/fonts/misc/cozette.bdf";
+    }
+  ];
+  esphome = pkgs.writeShellScriptBin "esphome" ''
+    set -eu
+    ${getExe pkgs.esphome} -s fonts_root "$ESPHOME_FONTS_ROOT" "$@"
+  '';
 in mkShell {
   nativeBuildInputs = [ esphome gnumake ];
+
+  ESPHOME_FONTS_ROOT = fonts;
 }
